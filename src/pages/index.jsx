c4e255c3c04e33e32267aeced3bd32133d40c558
kara03t <tomoki-kara@hotmail.co.jp>
2021-05-24 14:32:12 +0900

    ✨ useStateを使用したクリックイベントの追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,12 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 export default function Home() {
-  const foo = 1;
-  const handleClick= useCallback((e)=>{
-    console.log(e.target.href);
-    e.preventDefault()
-    alert(foo)
-  },[])
+  const [count, setCount] = useState(1)
+  const handleClick= (e)=>{
+   setCount((count)=>count + 1)
+  }
   useEffect(()=>{
     document.body.style.backgroundColor  ="lightblue"
     return ()=>{
@@ -24,9 +22,9 @@ export default function Home() {
         <title>Index page</title>
       </Head>
       <Header />
-      <a 
-      href="/about"
-      onClick={handleClick}>ボタン</a>
+      <h1>{count}</h1>
+      <button
+      onClick={handleClick}>ボタン</button>
       <Main page="index" />
 
       <Footer />
